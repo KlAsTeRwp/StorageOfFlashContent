6dadd47428c048413977e8dd1e3487a54ec72ac4
Nikita <lepesevich.nikita@yandex.ru>
2017-06-05 06:27:33 +0300

    created contex

---
+++ Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        DbSet<Content> Contents { get; set; }
+        DbSet<ContentDescription> ContentDescriptions { get; set; }
+        DbSet<ContentType> ContentTypes { get; set; }
+        DbSet<Rate> Rates { get; set; }
+
+        public EFDbContext(): base("DefaultConnection")
+        {
+        }
+    }
+}
