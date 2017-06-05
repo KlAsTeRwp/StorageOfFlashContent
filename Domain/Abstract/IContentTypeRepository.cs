b7be29d1db8eb2056116dee10482f57142e54a46
Nikita <lepesevich.nikita@yandex.ru>
2017-06-05 07:55:50 +0300

    added interface for ContentType repository and implemented it for Entity Framework

---
+++ Domain/Abstract/IContentTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Domain.Entities;
+
+//interface of content type repository
+namespace Domain.Abstract
+{
+    public interface IContentTypeRepository
+    {
+        //method for getting series of ContentType
+        IEnumerable<ContentType> ContentTypes { get; }
+        //method for adding new ContentType in database or modifing of issued
+        void Save(ContentType contentType);
+        //method for getting defined ContentType by primary key
+        ContentType Get(int id);
+        //method for deleting defined ContentType by primary key
+        ContentType Delete(int id);
+    }
+}
---
+++ Domain/Concrete/EFContentTypeRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFContentTypeRepository : IContentTypeRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<ContentType> ContentTypes => context.ContentTypes;
+
+        public ContentType Delete(int id)
+        {
+            ContentType result = null;
+            if (id > 0)
+            {
+                var dbEntry = context.ContentTypes.Find(id);
+                if (dbEntry != null)
+                {
+                    result = context.ContentTypes.Remove(dbEntry);
+                    context.SaveChanges();
+                }                
+            }
+            return result;
+        }
+
+        public ContentType Get(int id)
+        {
+            return context.ContentTypes.Find(id);
+        }
+
+        public void Save(ContentType contentType)
+        {
+            if (contentType != null && contentType.ID >= 0)
+            {
+                if (contentType.ID == 0)
+                {
+                    context.ContentTypes.Add(contentType);
+                }
+                if (contentType.ID > 0)
+                {
+                    var dbEntry = context.ContentTypes.Find(contentType.ID);
+                    if (dbEntry != null)
+                    {
+                        dbEntry.Type = contentType.Type;
+                        dbEntry.Extensions = contentType.Extensions;
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
