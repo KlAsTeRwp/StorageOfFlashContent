59972a7639472267246f04551adffc7dbcb8de6c
Nikita <lepesevich.nikita@yandex.ru>
2017-06-07 14:49:39 +0300

    addedd Home- AdminPanel- and Content controllers

---
+++ WebUI/Controllers/ContentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+
+namespace WebUI.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
