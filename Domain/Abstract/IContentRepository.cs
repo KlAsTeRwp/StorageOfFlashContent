23ca4fa3422d25102991dfffb2e67df585e8228f
Nikita <lepesevich.nikita@yandex.ru>
2017-06-08 07:50:07 +0300

    added interface for content repository and inmplementation for it

---
+++ Domain/Abstract/IContentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Domain.Entities;
+
+namespace Domain.Abstract
+{
+    public interface IContentRepository
+    {
+        //method for getting series of Content
+        IEnumerable<Content> Contents { get; }
+        //method for adding new Content in database or modifing of issued
+        void Save(Content content);
+        //method for getting defined Content by primary key
+        Content Get(int id);
+        //method for deleting defined Content by primary key
+        Content Delete(int id);
+    }
+}
---
+++ Domain/Concrete/EFContentRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFContentRepository : IContentRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Content> Contents => context.Contents;
+
+        public Content Delete(int id)
+        {
+            Content result = null;
+            if (id > 0)
+            {
+                var dbEntry = context.Contents.Find(id);
+                if (dbEntry != null)
+                {
+                    result = context.Contents.Remove(dbEntry);
+                    context.SaveChanges();
+                }
+            }
+            return result;
+        }
+
+        public Content Get(int id)
+        {
+            return context.Contents.Find(id);
+        }
+
+        public void Save(Content content)
+        {
+            if (content != null && content.ID >= 0)
+            {
+                if (content.ID == 0)
+                {
+                    context.Contents.Add(content);
+                }
+                if (content.ID > 0)
+                {
+                    var dbEntry = context.Contents.Find(content.ID);
+                    if (dbEntry != null)
+                    {
+                        dbEntry.Category = content.Category;
+                        dbEntry.CategoryID = content.CategoryID;
+                        dbEntry.ContentDescription = content.ContentDescription;
+                        dbEntry.ContentDescriptionID = content.ContentDescriptionID;
+                        dbEntry.ContentType = content.ContentType;
+                        dbEntry.ContentTypeID = content.ContentTypeID;
+                        dbEntry.Data = content.Data;
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
